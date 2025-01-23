87d9c82124a913537d3651e142a74cfd53829e0e
Roman Schauerte <121089503+romansch-hub@users.noreply.github.com>
2025-01-23 11:16:36 +0100

    Added line.sh
    Processing of the modbam2bed output

---
+++ line.sh
@@ -0,0 +1,19 @@
+timestamp(){
+	date +"%T"
+}
+
+timestamp
+
+while IFS= read -r line
+	do 
+		#echo "$line"
+		#python3 filter_modbam.py -i mid.bed -o out2.bed
+		#rm mid.bed
+		awk '{if ($11 == "nan") $11 = 0;
+        		if ($12 == "nan") $12 = 0;
+        		if ($13 == "nan") $13 = 0;
+        		print $1"\t"$2"\t"$3"\t"$11"\t"$13"\t"$12}' > out.bed
+	done < mod-counts.bed
+
+
+timestamp
